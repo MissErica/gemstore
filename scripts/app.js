90ab441ab7bfb26777f0616074612813d06a30cf
MissErica <Erica@Ericas-MacBook-Pro.local>
2017-02-28 15:19:22 -0600

    added review submit

--- scripts/app.js
+++ scripts/app.js
@@ -18,8 +18,24 @@ var gems = [{
         {
             fullSize: '/images/gem-02.gif'
         },
-    ]
-    
+    ],
+    reviews: [
+        {
+            stars: 5,
+            body: "I love this product",
+            author:"erica@gmail.com"
+        },
+        {
+            stars: 3,
+            body: "this was ok",
+            author: "jon"
+        },
+        {
+            stars:4,
+            body:"this was lovely",
+            author:"dorothy from ohio"
+        },
+    ],
 }, 
            {
              name: 'Pentagonal',
@@ -82,7 +98,7 @@ var gems = [{
                
            },
             {
-             name: 'Turquoise',
+             name: 'Ruby',
             price: 8.95,
             description: 'Best seller!',
             canPurchase: true,  
@@ -121,7 +137,11 @@ myApp.controller('myController', function($scope) {
 myApp.controller('panelController', function($scope){
     $scope.tab = 1;
     $scope.selectTab = function(tab){
-        $scope.tab = tab;
+        if(tab == $scope.tab){
+            $scope.tab = -1
+        } else {
+            $scope.tab = tab;
+        }
         console.log("tab changed to "+ tab)
     };
     
@@ -129,3 +149,18 @@ myApp.controller('panelController', function($scope){
         return $scope.tab === tab;
     };
 });
+
+
+myApp.controller("reviewController",function($scope){
+    $scope.review={};
+    
+    $scope.addReview= function(product){
+        if(!product.reviews) {
+            product.reviews = [];
+        }
+        product.reviews.push($scope.review);
+        //TODO: send this review to our API so it can be saved server-side
+        $scope.review = {};
+    };
+});
+
