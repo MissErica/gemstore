70b27a9f1de81865da2d1eb16acec2c8f46fd52c
MissErica <Erica@Ericas-MacBook-Pro.local>
2017-03-01 13:38:13 -0600

    made modules

--- scripts/app.js
+++ scripts/app.js
@@ -1,178 +1,121 @@
-
 $(function () {
-  $('[data-toggle="popover"]').popover()
+    $('[data-toggle="popover"]').popover()
 })
 
-var myApp = angular.module('myApp', ['ui.bootstrap']);
-
-
+var myApp = angular.module('myApp', ['ui.bootstrap', 'gemProducts']);
 var gems = [{
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have hidden qualities beyond their lustre, beyond their shine...Dodecahedron is one of those gems',
-    canPurchase: true,
-    images:[
-        {
-            fullSize: '/images/gem-01.gif'
-        },
-        {
-            fullSize: '/images/gem-02.gif'
-        },
-    ],
-    reviews: [
-        {
-            stars: 5,
-            body: "I love this product",
-            author:"erica@gmail.com"
-        },
-        {
-            stars: 3,
-            body: "this was ok",
-            author: "jon"
-        },
-        {
-            stars:4,
-            body:"this was lovely",
-            author:"dorothy from ohio"
-        },
-    ],
-}, 
-           {
-             name: 'Pentagonal',
-            price: 5.95,
-            description: 'Best seller!',
-            canPurchase: true, 
-            images:[
-        {
-            fullSize: '/images/gem-03.gif'
-        },
-        {
-            fullSize: '/images/gem-04.gif'
-        },
-    ]
-               
-           },
+        name: 'Dodecahedron'
+        , price: 2.95
+        , description: 'Some gems have hidden qualities beyond their lustre, beyond their shine...Dodecahedron is one of those gems'
+        , canPurchase: true
+        , images: [
             {
-             name: 'Hexagonal',
-            price: 12.95,
-            description: 'Best seller!',
-            canPurchase: true,  
-            images:[
-        {
-            fullSize: '/images/gem-05.gif'
-        },
-        {
-            fullSize: '/images/gem-06.gif'
-        },
-    ]    
-               
-           },
+                fullSize: '/images/gem-01.gif'
+        }
+            , {
+                fullSize: '/images/gem-02.gif'
+        }
+    , ]
+        , reviews: [
             {
-             name: 'Azurite',
-            price: 7.95,
-            description: 'Best seller!',
-            canPurchase: true,
-                images:[
-        {
-            fullSize: '/images/gem-07.gif'
-        },
-        {
-            fullSize: '/images/gem-08.gif'
-        },
-    ]
-               
-           },
+                stars: 5
+                , body: "I love this product"
+                , author: "erica@gmail.com"
+        }
+            , {
+                stars: 3
+                , body: "this was ok"
+                , author: "jon"
+        }
+            , {
+                stars: 4
+                , body: "this was lovely"
+                , author: "dorothy from ohio"
+        }
+    , ]
+, }
+    , {
+        name: 'Pentagonal'
+        , price: 5.95
+        , description: 'Best seller!'
+        , canPurchase: true
+        , images: [
             {
-             name: 'Feldspar',
-            price: 2.95,
-            description: 'Best seller!',
-            canPurchase: true, 
-                images:[
-        {
-            fullSize: '/images/gem-09.gif'
-        },
-        {
-            fullSize: '/images/gem-10.gif'
-        },
-    ]
-               
-           },
-            {
-             name: 'Ruby',
-            price: 8.95,
-            description: 'Best seller!',
-            canPurchase: true,  
-                images:[
-        {
-            fullSize: '/images/gem-01.gif'
-        },
-        {
-            fullSize: '/images/gem-02.gif'
-        },
-    ]
-               
+                fullSize: '/images/gem-03.gif'
+        }
+            , {
+                fullSize: '/images/gem-04.gif'
+        }
+    , ]
            }
-        
+    , {
+        name: 'Hexagonal'
+        , price: 12.95
+        , description: 'Best seller!'
+        , canPurchase: true
+        , images: [
+            {
+                fullSize: '/images/gem-05.gif'
+        }
+            , {
+                fullSize: '/images/gem-06.gif'
+        }
+    , ]
+           }
+    , {
+        name: 'Azurite'
+        , price: 7.95
+        , description: 'Best seller!'
+        , canPurchase: true
+        , images: [
+            {
+                fullSize: '/images/gem-07.gif'
+        }
+            , {
+                fullSize: '/images/gem-08.gif'
+        }
+    , ]
+           }
+    , {
+        name: 'Feldspar'
+        , price: 2.95
+        , description: 'Best seller!'
+        , canPurchase: true
+        , images: [
+            {
+                fullSize: '/images/gem-09.gif'
+        }
+            , {
+                fullSize: '/images/gem-10.gif'
+        }
+    , ]
+           }
+    , {
+        name: 'Ruby'
+        , price: 8.95
+        , description: 'Best seller!'
+        , canPurchase: true
+        , images: [
+            {
+                fullSize: '/images/gem-01.gif'
+        }
+            , {
+                fullSize: '/images/gem-02.gif'
+        }
+    , ]
+           }
+
           ]
-
-
-
-myApp.controller('myController', function($scope) {
-    $scope.cart=[];
+myApp.controller('myController', function ($scope) {
+    $scope.cart = [];
     $scope.model = gems;
-    
-    $scope.initialize = function() {
+    $scope.initialize = function () {
         console.log("Initilized");
     }
-    
-    $scope.addProduct = function(product){
+    $scope.addProduct = function (product) {
         $scope.cart.push(product);
         console.log("product added")
     }
-
 });
 
-
-
-myApp.controller('panelController', function($scope){
-    $scope.tab = 1;
-    $scope.selectTab = function(tab){
-        if(tab == $scope.tab){
-            $scope.tab = -1
-        } else {
-            $scope.tab = tab;
-        }
-        console.log("tab changed to "+ tab)
-    };
-    
-    $scope.isSelected = function(tab){
-        return $scope.tab === tab;
-    };
-});
-
-
-myApp.controller("reviewController",function($scope){
-    $scope.review={};
-    
-    $scope.addReview= function(product){
-        if(!product.reviews) {
-            product.reviews = [];
-        }
-        product.reviews.push($scope.review);
-        //TODO: send this review to our API so it can be saved server-side
-        $scope.review = {};
-        //This resets the form after submission so it doesn't show error 
-        $scope.reviewForm.body.$setPristine();
-        $scope.reviewForm.author.$setPrisitne();
-        $scope.reviewForm.stars.$setPristine();
-    }
-    $scope.validate = function(){
-        //Touching each of the inputs in the current form to set them to dirty
-        //If they've been left blank, this will cause the has-error class to be applied
-        $scope.reviewForm.body.$setDirty();
-        $scope.reviewForm.author.$setDirty();
-        $scope.reviewForm.stars.$setDirty();
-        return $scope.reviewForm.$valid;
-    }
-});
-
+            });
---
+++ scripts/modules/gemProducts.js
@@ -0,0 +1,60 @@
+
+angular.module("gemProducts", []); //this line defines the new directive
+
+
+
+
+
+//using angular.module with one parameter will "fetch" the defined directive from the angular runtime, and you can add new directives/controllers to it
+//this will be converted to snake case: gen-write-review
+angular.module("gemProducts").directive("gemWriteReview", function () {
+            return {
+                restrict: 'A'
+                , templateUrl: '/templates/gem-write-review.html'
+                , controller: function ($scope) {
+                    $scope.review = {};
+                    $scope.addReview = function (product) {
+                        if (!product.reviews) {
+                            product.reviews = [];
+                        }
+                        product.reviews.push($scope.review);
+                        //TODO: send this review to our API so it can be saved server-side
+                        $scope.review = {};
+                        //This resets the form after submission so it doesn't show error 
+                        $scope.reviewForm.body.$setPristine();
+                        $scope.reviewForm.author.$setPrisitne();
+                        $scope.reviewForm.stars.$setPristine();
+                    }
+                    $scope.validate = function () {
+                        //Touching each of the inputs in the current form to set them to dirty
+                        //If they've been left blank, this will cause the has-error class to be applied
+                        $scope.reviewForm.body.$setDirty();
+                        $scope.reviewForm.author.$setDirty();
+                        $scope.reviewForm.stars.$setDirty();
+                        return $scope.reviewForm.$valid;
+                    }
+                }
+            }
+})
+
+
+
+ angular.module("gemProducts").directive("gemPanels", function () {
+                return {
+                    restrict: 'A',
+                     templateUrl: 'templates/gem-panels.html',
+                     controller: function ($scope) {
+                        $scope.tab = 1;
+                        $scope.selectTab = function (tab) {
+                            if (tab == $scope.tab) {
+                                $scope.tab = -1;
+                            }
+                            else {
+                                $scope.tab = tab;
+                            }
+                        }
+                        $scope.isSelected = function (tab) {
+                            return $scope.tab === tab;
+                        }
+                    }
+                }
